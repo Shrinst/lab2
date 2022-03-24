1708a2e4f86d611ff73913d2fd82e6e4afa10e1b
Shrinst <wodofwarras1@gmail.com>
2022-03-23 22:31:26 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,48 +1,19 @@
-# Generates an archive of the source code compressed as a .zip file.
-data "archive_file" "source" {
-    type        = "zip"
-    source_dir  = "../src"
-    output_path = "/tmp/function.zip"
-}
-
-# Add source code zip to the Cloud Function's bucket
-resource "google_storage_bucket_object" "zip" {
-    source       = data.archive_file.source.output_path
-    content_type = "application/zip"
-
-    # Append to the MD5 checksum of the files's content
-    # to force the zip to be updated as soon as a change occurs
-    name         = "src-${data.archive_file.source.output_md5}.zip"
-    bucket       = google_storage_bucket.function_bucket.name
-
-    # Dependencies are automatically inferred so these lines can be deleted
-    depends_on   = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
-        data.archive_file.source
-    ]
-}
-
-# Create the Cloud function triggered by a `Finalize` event on the bucket
-resource "google_cloudfunctions_function" "function" {
-    name                  = "function-trigger-on-gcs"
-    runtime               = "python37"  # of course changeable
-
-    # Get the source code of the cloud function as a Zip compression
-    source_archive_bucket = google_storage_bucket.function_bucket.name
-    source_archive_object = google_storage_bucket_object.zip.name
-
-    # Must match the function name in the cloud function `main.py` source code
-    entry_point           = "hello_gcs"
-    
-    # 
-    event_trigger {
-        event_type = "google.storage.object.finalize"
-        resource   = "${var.project_id}-input"
-    }
-
-    # Dependencies are automatically inferred so these lines can be deleted
-    depends_on            = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
-        google_storage_bucket_object.zip
-    ]
-}
+def hello_gcs(event, context):
+    """Background Cloud Function to be triggered by Cloud Storage.
+       This generic function logs relevant data when a file is changed.
+    Args:
+        event (dict):  The dictionary with data specific to this type of event.
+                       The `data` field contains a description of the event in
+                       the Cloud Storage `object` format described here:
+                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to Stackdriver Logging
+    """
+    print('Event ID:' , context.event_id)
+    print('Event type:', context.event_type)
+    print('Bucket:', event['bucket'])
+    print('File:',  event['name'])
+    print('Metageneration:',  event['metageneration'])
+    print('Created:',  event['timeCreated'])
+    print('Updated:',  event['updated'])
